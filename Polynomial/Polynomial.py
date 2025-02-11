964e3cf060314967b4f2285db84baffa960edb21
Dylan C <dylanjohncolligan@gmail.com>
2025-02-11 15:31:44 +0000

    massive W

--- Polynomial/Polynomial.py
+++ Polynomial/Polynomial.py
@@ -4,9 +4,14 @@ import matplotlib.pyplot as plt
 
 class polynomial_trend:
     def __init__(self, filename, order=0, new=False):
+        """
+        use order to set generated data trend, 2 for quadratic, 3 for cubic etc
+        filename is where to save to or where to read from
+        new is whether to make a new one, set to false auto if order=0
+        """
         self.csv_file = filename
         if order != 0 and new:
-            self.generate_poly_data(order, noise_level=100)
+            self.generate_poly_data(order)
         df = pd.read_csv(self.csv_file)
         self.xs = df.iloc[:, 0]
         self.ys = df.iloc[:, 1]
@@ -14,30 +19,41 @@ class polynomial_trend:
         self.numpy_ys = np.array(self.ys)
         self.plot_data()
 
-    def generate_poly_data(self, order, num_points=100, noise_level=1.5):
-        coefficients=np.random.normal(0, 1, order + 1)
-        x = np.linspace(-5, 5, num_points)
+    def generate_poly_data(self, order, num_points=4):
+        """
+        the added noise is order ** 2
+        """
+        coefficients=np.random.normal(0, 1.5, order + 1)
+        x = np.linspace(-3, 3, num_points)
         y = np.sum([c * (x**i) for i, c in enumerate(coefficients)], axis=0)
-        noise = np.random.normal(0, noise_level, num_points)
+        noise = np.random.normal(0, order**2, num_points)
         y += noise
         df = pd.DataFrame({'x': x, 'y': y})
         df.to_csv(self.csv_file, index=False)
     
-    def plot_data(self, coefficients=None, extra=0):
+    def plot_data(self, coefficients=None, extra=0, mse=0):
+        """
+        extra is short for extrapolation
+        mse is on title
+        if coefficients is none, no line of best fit
+        """
         plt.scatter(self.xs, self.ys)
         if coefficients is not None:
             poly_func = np.poly1d(coefficients[::-1])
             x_range = np.linspace(min(self.xs), max(self.xs) + extra, 100)
             y_fit = poly_func(x_range)
-            plt.plot(x_range, y_fit, color='red', label="Polynomial Fit")
+            plt.plot(x_range, y_fit, color='red')
+            plt.title(f"Relative MSE: {np.round((mse /(max(self.xs) - min(self.xs))), 2)}\ny = " + " + ".join(f"{np.round(c, 2)} * x^{i}" for i, c in enumerate(coefficients)))
         plt.show()
     
-    def regress(self, order, rate=0.000001, tolerance=0.01, max_iterations=1000):
-        coefficients = np.random.normal(0, 1, order + 1)
-        
+    def regress(self, order, rate=0.0000005, tolerance=0.0001, max_iterations=25000):
+        """
+        honestly not that great, really good at cubic but quite bad at anything < 3
+        """
+        coefficients = np.random.normal(0, pow(max(self.xs) - min(self.xs), (1/order)), order + 1)
+        #randomise coefficients
         last_mse = self.MSE(coefficients)
         iteration = 0
-
         while iteration < max_iterations:
             poly_func = np.poly1d(coefficients[::-1])
             predicted_ys = poly_func(self.numpy_xs)
@@ -48,19 +64,35 @@ class polynomial_trend:
             coefficients -= rate * gradient
             current_mse = self.MSE(coefficients)
             if abs(last_mse - current_mse) < tolerance:
-                print(f"Converged after {iteration} iterations.")
+                print(f"MSE: {current_mse}")
                 break
             last_mse = current_mse
             iteration += 1
-            if iteration % 100 == 0:
-                print(f"Iteration {iteration}: MSE = {current_mse}")
-        return coefficients
+        return coefficients, current_mse
     
     def MSE(self, coefficients):
-        predictions = np.polyval(coefficients[::-1], self.xs)
-        errors = (predictions - self.ys) ** 2
+        poly_func = np.poly1d(coefficients[::-1])
+        predicted_ys = poly_func(self.numpy_xs)
+        errors = (predicted_ys - self.numpy_ys) ** 2
         return np.mean(errors)
 
-newtrend = polynomial_trend('.\Polynomial\sample_data.csv', 3, True)
-cos = newtrend.regress(3)
-newtrend.plot_data(cos, 2)
+    def general_regress(self):
+        best_coefficients, best_mse = self.regress(1)
+        bestBIC = self.BIC(best_coefficients, best_mse)
+        datapoints = len(self.xs)
+        for i in range(1, int(np.round(np.sqrt(datapoints), 0)) + 1):
+            coeffs, mse = self.regress(i)
+            newtrend.plot_data(coeffs, mse=mse)
+            current_BIC = self.BIC(coeffs, mse)
+            if current_BIC < bestBIC:
+                best_coefficients, best_mse = coeffs, mse
+                bestBIC = current_BIC
+        return best_coefficients, best_mse
+    
+    def BIC(self, coeffs, mse):
+        n = len(self.xs)
+        k = len(coeffs) 
+        return (n * np.log(mse)) + (k * np.log(n))
+
+newtrend = polynomial_trend(r'.\Polynomial\sampledata.csv', np.random.randint(2, 4), True)
+cos, mse = newtrend.general_regress()
